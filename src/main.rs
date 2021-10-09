0cc4e8f21c380f7372d0ab1b98e6dbbb451eda97
Alessandro <alessandro.angioi@hotmail.com>
2021-10-09 20:24:57 +0200

    Implement annealing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use rand::seq::SliceRandom;
+use rand::{
+    seq::SliceRandom,
+    Rng
+};
 
 type Spin = i8;
 type GridCoordinate = i8;
@@ -51,7 +54,6 @@ impl IsingModel {
     fn get_spin(&self, i: GridCoordinate, j: GridCoordinate) -> &Spin {
         let x = if i>=0 {i % self.width}  else { self.width  + i % self.width };
         let y = if j>=0 {j % self.height} else { self.height + j % self.height };
-        println!("{},{}", x, y);
         &self.spins[(x + self.width * y) as usize]
     }
 
@@ -67,12 +69,55 @@ impl IsingModel {
         let j = rng.gen_range(0..self.height);
         (i, j)
     }
+
+    fn calculate_energy_contribution(&self, i: GridCoordinate, j: GridCoordinate) -> f32 {
+        let mut neighbors_spin_sum: Spin = 0;
+        for offset in Self::OFFSETS {
+            neighbors_spin_sum += self.get_spin(i + offset.x, j + offset.y)
+        }
+        self.coupling_constant * (self.get_spin(i, j) * neighbors_spin_sum) as f32
+    }
+
+    fn total_energy(&self) -> f32 {
+        // Room for improvements, naive loop
+        let mut total: f32 = 0.;
+        for i in 0..self.width {
+            for j in 0..self.height {
+                total += self.calculate_energy_contribution(i, j);
+            }
+        }
+        total
+    }
+
+    fn calculate_boltzmann_factor(energy_contribution: f32, temperature: f32) -> f32 {
+        (2.0*energy_contribution/temperature).exp()
+    }
+
+    fn step(&mut self, temperature: f32) -> Option<(GridCoordinate, GridCoordinate)> {
+        let (i, j) = self.select_random_node();
+
+        let energy_contribution = self.calculate_energy_contribution(i, j);
+
+        let spin_should_flip = energy_contribution >= 0.0  
+            || rand::thread_rng().gen::<f32>() < Self::calculate_boltzmann_factor(energy_contribution, temperature);
+
+        if spin_should_flip {
+            println!("Accepted");
+            self.flip_spin(i, j);
+            return Some((i, j))
+        } else {
+            println!("Rejected");
+            None
+        }
+    }
+
+    
 }
 
 fn main() {
-    let model = IsingModel::new(3, 4, (2f32.sqrt() + 1.).ln() / 2.);
-    println!(
-        "{}, {}, {}, {:?}",
-        model.width, model.height, model.coupling_constant, model.spins
-    );
+    let mut model = IsingModel::new(10, 10, (2f32.sqrt() + 1.).ln() / 2.);
+    for i in 0..3000 {
+        model.step(0.1);
+        println!("{}", model.total_energy())
+    }
 }
